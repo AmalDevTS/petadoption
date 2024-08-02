1f971ba83de80bf371fbac4f63df2cffbaedb75a
AmalDevTS <amal60266@gmail.com>
2024-08-02 21:51:55 +0530

    petadoption website using react, redux

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,37 +7,26 @@ import Swal from 'sweetalert2';
 import catimage from '../images/60-607569_cat-and-dog-png-no-background-description-of.png'
 import logoimg from '../images/646-6464701_this-free-icons-png-design-of-dog-and.png'
 
-
-
-
-
-
 function Footer() {
-
   const form = useRef();
+
   const sendEmail = (e) => {
-    Swal.fire({
-      icon: 'success',
-      title: 'Email Sent Successfully',
-      showConfirmButton: false,
-      timer: 1500
-    });
-
-
-
-
     e.preventDefault();
-
 
     emailjs.sendForm('service_s3pdj0h', 'template_57kbbhd', form.current, 'LUmjhQENdbPc6fTce')
       .then((result) => {
         console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Email Sent Successfully',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        form.current.reset();
       }, (error) => {
         console.log(error.text);
       });
-    }
-  
- 
+  }
 
   return (
     <div className="footer-container bg-warning py-5 mt-5">
@@ -45,9 +34,9 @@ function Footer() {
         <Row className="justify-content-center align-items-center">
           <Col md={3}>
             <Link to={'/'} className="text-decoration-none text-dark">
-              <h5 className="logo"><img src={logoimg} style={{width:"45px"}} alt="" /> Pet Nest</h5>
+              <h5 className="logo"><img src={logoimg} style={{ width: "45px" }} alt="" /> Pet Nest</h5>
             </Link>
-            <img src={catimage} style={{width:"80%"}} alt="Pet Nest Logo" className="logo-image img-fluid" />
+            <img src={catimage} style={{ width: "80%" }} alt="Pet Nest Logo" className="logo-image img-fluid" />
           </Col>
 
           <Col md={3}>
@@ -67,22 +56,18 @@ function Footer() {
           <Col md={3}>
             <h4>Contact us</h4>
             <Form ref={form} onSubmit={sendEmail}>
-
-            <Form.Group className="mb-3">
-            <Form.Control type="text" placeholder="Name" name='from_name' />
-
-            </Form.Group>
-            <Form.Group className="mb-3">
-            <Form.Control type="email" placeholder="Enter your email" name='from_email' />
-
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control type="text" placeholder="Message" name='message'  />
-            </Form.Group>
-            <Button style={{ backgroundColor: "#4b1c81", borderRadius: "20px", borderColor: "#4b1c81" }} variant="primary" type='submit' >Submit</Button>
-
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Name" name='from_name' />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control type="email" placeholder="Enter your email" name='from_email' />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Message" name='message' />
+              </Form.Group>
+              <Button style={{ backgroundColor: "#4b1c81", borderRadius: "20px", borderColor: "#4b1c81" }} variant="primary" type='submit'>Submit</Button>
             </Form>
-            
+
             <div className="social-icons mt-3">
               <i className="fab fa-facebook fa-2x me-3"></i>
               <i className="fab fa-linkedin fa-2x me-3"></i>
@@ -97,4 +82,4 @@ function Footer() {
   );
 }
 
-export default Footer
+export default Footer;
